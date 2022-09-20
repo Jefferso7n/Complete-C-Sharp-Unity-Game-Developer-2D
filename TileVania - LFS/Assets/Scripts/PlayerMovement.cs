bb42e09619e50b24fd9b113b37e8e4cd2743b04e
Jefferso7n <jeffersonfillipe2021@gmail.com>
2022-09-19 22:41:04 -0300

    Added TileVania Game
    Unfinished.

---
+++ TileVania - LFS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+
+    [SerializeField] Rigidbody2D myRigidbody2D;
+    [SerializeField] CapsuleCollider2D myCapsuleCollider2D;
+    [SerializeField] Animator myAnimator;
+
+    Vector2 moveInput;
+    float gravityScaleAtStart;
+
+    void Start()
+    {
+        gravityScaleAtStart = myRigidbody2D.gravityScale;
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        ClimbLadder();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (value.isPressed)
+        {
+            myRigidbody2D.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody2D.velocity.y);
+        myRigidbody2D.velocity = playerVelocity;
+
+        ChangeRunningAnimation();
+    }
+
+    void FlipSprite()
+    {
+        if (CheckPlayerHorizontalMovement())
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidbody2D.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("isClimbing", false);
+            return;
+        }
+
+        Vector2 climbVelocity = new Vector2(myRigidbody2D.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody2D.velocity = climbVelocity;
+        myRigidbody2D.gravityScale = 0f;
+
+        ChangeClimbingAnimation();
+    }
+
+    bool CheckPlayerHorizontalMovement()
+    {
+        return Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+    }
+
+    bool CheckPlayerVerticalMovement()
+    {
+        return Mathf.Abs(myRigidbody2D.velocity.y) > Mathf.Epsilon;
+    }
+
+    void ChangeRunningAnimation()
+    {
+        myAnimator.SetBool("isRunning", CheckPlayerHorizontalMovement());
+
+        // Another way to do this
+        // if (CheckPlayerHorizontalMovement())
+        // {
+        //     myAnimator.SetBool("isRunning", true);
+        // }
+        // else
+        // {
+        //     myAnimator.SetBool("isRunning", false);
+        // }
+    }
+
+    void ChangeClimbingAnimation()
+    {
+        myAnimator.SetBool("isClimbing", CheckPlayerVerticalMovement());
+
+        // Another way to do this
+        // if (CheckPlayerVerticalMovement())
+        // {
+        //     myAnimator.SetBool("isClimbing", true);
+        // }
+        // else
+        // {
+        //     myAnimator.SetBool("isClimbing", false);
+        // }
+    }
+
+}
